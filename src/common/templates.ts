930607b75d2bada8f3540cf716d28f901810308b
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-11-21 18:16:47 +0900

    テンプレート1を作成

--- src/common/templates.ts
+++ src/common/templates.ts
@@ -1,17 +1,51 @@
 //xml形式でテンプレートを作成できる
 export const templates = [
   {
-    name: "While True Template",
+    name: "Template 1",
     blockxml: `
       <block type="custom_while_true">
         <statement name="STATEMENTS">
           <block type="custom_common_if_else">
             <value name="EXPRESSION">
+              <block type="custom_maze_checkWall">
+                <field name="DIRECTION">left</field>
+              </block>
             </value>
             <statement name="TRUE_STATEMENTS">
+              <block type="custom_common_if">
+                <value name="EXPRESSION">
+                  <block type="custom_maze_checkWall">
+                    <field name="DIRECTION">top</field>
+                  </block>
+                </value>
+                <statement name="TRUE_STATEMENTS">
+                  <block type="custom_common_if_else">
+                    <value name="EXPRESSION">
+                      <block type="custom_maze_checkWall">
+                        <field name="DIRECTION">right</field>
+                      </block>
+                    </value>
+                    <statement name="TRUE_STATEMENTS">
+
+                    </statement>
+                    <statement name="FALSE_STATEMENTS">
+
+                    </statement>
+                  </block>
+                </statement>
+                <statement name="FALSE_STATEMENTS">
+
+                </statement>
+              </block>
             </statement>
             <statement name="FALSE_STATEMENTS">
-            </statement>
+            <block type="custom_maze_turn">
+              <field name="DIRECTION">left</field>
+            </block>
+          </statement>
+            <next>
+              <block type="custom_maze_stepForward"></block>
+            </next>
           </block>
         </statement>
       </block>
